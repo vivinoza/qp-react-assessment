5e7d252205dcfa04ace9f3423a056f7043e10f59
Vivin Oza <vivin.oza@gmail.com>
2024-05-10 00:35:00 +0530

    Added add task and delete task api calls in FE

--- task-manager/src/context/TasksContext.tsx
+++ task-manager/src/context/TasksContext.tsx
@@ -6,11 +6,15 @@ const initialValue: {
   tasks: Task[];
   isLoading: boolean;
   updateStatus: (completed: boolean, id: string) => void;
+  addTask: (title: string, description: string) => void;
+  deleteTask: (id: string) => void;
 } = {
   getTasks: () => {},
   tasks: [],
   isLoading: false,
   updateStatus: () => {},
+  addTask: () => {},
+  deleteTask: () => {},
 };
 
 const TasksPageContext = createContext({ ...initialValue });
@@ -56,9 +60,37 @@ export const TasksPageContextProvider = (props: any) => {
     return response.json();
   };
 
+  const addTask = async (title: string, description: string) => {
+    const response = await fetch(`${host}/api/tasks/addtask`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, description }),
+    });
+    const task = await response.json();
+    setTasks(tasks.concat(task));
+  };
+
+  const deleteTask = async (id: string) => {
+    const response = await fetch(`${host}/api/tasks/deletetask/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEzMWRjNWUzZTQwMzdjZDQ3MzRhMDY2In0sImlhdCI6MTYzMDY2OTU5Nn0.hJS0hx6I7ROugkqjL2CjrJuefA3pJi-IU5yGUbRHI4Q",
+      },
+    });
+    const json = response.json();
+    const newTasks = tasks.filter((task) => {
+      return task._id !== id;
+    });
+    setTasks(newTasks);
+  };
+
   return (
     <TasksPageContext.Provider
-      value={{ getTasks, tasks, isLoading, updateStatus }}
+      value={{ getTasks, tasks, isLoading, updateStatus, addTask, deleteTask }}
     >
       {props.children}
     </TasksPageContext.Provider>
